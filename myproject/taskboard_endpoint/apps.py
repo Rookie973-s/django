e13c0bf39c148b14fd34cc220f1223da9435f3c4
AG <richfiledadupke@gmail.com>
2026-06-05 07:29:04 -0700

    Delete myproject directory

--- myproject/taskboard_endpoint/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskboardEndpointConfig(AppConfig):
-    name = 'taskboard_endpoint'
--- myproject/taskboard_endpoint/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import include, path
-from .views import *
-urlpatterns = [
-    path("create_task/", create_task)   
-]
